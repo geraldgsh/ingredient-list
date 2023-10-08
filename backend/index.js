4b32eff22928b9c031c5446831c3659c21681c26
geraldgsh <29500109+geraldgsh@users.noreply.github.com>
2023-10-08 16:59:21 +0800

    add POST

--- backend/index.js
+++ backend/index.js
@@ -7,21 +7,39 @@ app.use(express.json());
 app.use(cors())
 
 app.get("/ingredients", (req, res) => {
-    fetch(
-        "https://react-hooks-update-d8f72-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json")
-        .then((response) => response.json())
-        .then((responseData) => {
-          const loadedIngredients = [];
-          for (const key in responseData) {
-            loadedIngredients.push({
-              id: key,
-              title: responseData[key].title,
-              description: responseData[key].description,
-              amount: responseData[key].amount
-            });
-          }
-          res.json(loadedIngredients);
+  fetch(
+    "https://react-hooks-update-d8f72-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json")
+    .then((response) => response.json())
+    .then((responseData) => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          description: responseData[key].description,
+          amount: responseData[key].amount
         });
+      }
+      res.json(loadedIngredients);
+    });
+});
+
+app.post("/ingredients", async (req, res) => {
+  const ingredient = req?.body;
+  fetch(
+    "https://react-hooks-update-d8f72-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json",
+    {
+      method: "POST",
+      body: JSON.stringify(ingredient),
+      headers: { "Content-Type": "application/json" }
+    }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((responseData) => {
+      res.status(201).json(responseData);
+    });
 });
 
 app.listen(5000, () => console.log("API server is running..."));
